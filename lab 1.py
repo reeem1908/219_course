0860a1ea8be2cf9e0e3a3cf8b2d3955bfeccae6f
reeem1908 <89980557+reeem1908@users.noreply.github.com>
2021-09-02 15:13:00 +0300

    Add files via upload

---
+++ lab 1.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  1 08:41:58 2021
+
+@author: Maximus
+"""
+
+import math
+
+type(math.factorial(10))
+
+math.pi
+
+Hi = '''hello and good morning'''
+print(Hi)
+
+list8=list(range(0, 351)) 
+print(list8)
+list8[60]
+
+
+list1= list(range(0, 147, 7))
+list1[-20]
+
+print(list1)
+
+-----------
+list(range(0, 147, 7))
+
+list1= list(range(0, 147, 7))
+print(list1)
+
+sublist=list8[10:101]
+Print(sublist)
+
+mylist2D = []
+y1=['true', 'R', math.pi, math.e, '2023', '1908']
+mylist2D.append(y1)
+y1=['flase', 'A', math.sigma, math.factorial, '2001', 'Aug']
+mylist2D.append(y1)
